0780942a753e2914b21634f0229ae92dd90674e4
Ayxa Chaverra <achaverrar@gmail.com>
2023-03-27 09:05:14 -0500

    break the sections down into react components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
