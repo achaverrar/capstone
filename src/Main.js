0259297e91652377ad0486e70f308f468603d3e0
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-10 09:55:07 -0500

    complete the specials section's markup

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import Specials from "./Specials";
 
 const Main = () => {
   return (
     <main>
       <Hero></Hero>
+      <Specials></Specials>
     </main>
   );
 };
