476d05e592a6b6f53fc393eaae621e9c76254e46
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-11 22:28:21 -0500

    complete the desktop version of the about us section

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import Specials from "./Specials";
 import Testimonials from "./Testimonials";
+import AboutUs from "./AboutUs";
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <Hero></Hero>
       <Specials></Specials>
       <Testimonials></Testimonials>
+      <AboutUs></AboutUs>
     </main>
   );
 };
